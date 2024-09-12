b4a4374e110678242f87bdbc67e5518760de9e84
devanshchoudhary20 <88880159+devanshchoudhary20@users.noreply.github.com>
2024-09-12 11:28:33 +0530

    Update footer.jsx

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
     <div>
         <footer className="flex justify-between  bg-gray-800 text-white pt-6 px-6 w-full">
     <div className='flex flex-col justify-start items-start space-y-2'>
-      <a href="https://www.linkedin.com/in/devansh-choudhary-2381041b1/" className="mr-4 hover:text-gray-300" target="_blank">LinkedIn</a>
-      <a href="https://github.com/devanshchoudhary20/BIZPrize" className="hover:text-gray-300" target="_blank">Github</a>
+      <a href="https://www.linkedin.com/in/devansh-choudhary-2381041b1/" className="mr-4 hover:text-gray-300" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="https://github.com/devanshchoudhary20/BIZPrize" className="hover:text-gray-300" target="_blank" rel="noopener noreferrer">Github</a>
     </div>
     <img src="./images/splash.png" alt="Colorful splash" className="h-60 w-auto"  />
   </footer>
